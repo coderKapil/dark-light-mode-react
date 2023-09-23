51b9480e6d1b02c2dde080a369cf97ea70a986d8
Kapil Rana <kapsrana56@gmail.com>
2023-09-23 11:05:44 +0530

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import DarkAndLightMode from "./Components/DarkAndLightMode";
+
+function App() {
+  return (
+    <>
+      <div>
+        <DarkAndLightMode />
+      </div>
+    </>
+  );
+}
+
+export default App;
